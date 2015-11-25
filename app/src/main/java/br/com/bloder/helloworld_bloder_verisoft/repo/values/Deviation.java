f194ffa67f14c625ed9c85a497a56745caa43646
Daniel Leme <daniel.leme@verisoft.com.br>
2015-11-25 16:45:48 -0200

    DeviationList and Deviation json models implemented

---
+++ app/src/main/java/br/com/bloder/helloworld_bloder_verisoft/repo/values/Deviation.java
@@ -0,0 +1,10 @@
+package br.com.bloder.helloworld_bloder_verisoft.repo.values;
+
+/**
+ * Created by denis on 25/11/15.
+ */
+public class Deviation {
+
+    public String title;
+
+}
---
+++ app/src/main/java/br/com/bloder/helloworld_bloder_verisoft/repo/values/DeviationList.java
@@ -0,0 +1,16 @@
+package br.com.bloder.helloworld_bloder_verisoft.repo.values;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by denis on 25/11/15.
+ */
+public class DeviationList {
+
+    public List<Deviation> deviationList = new ArrayList<>();
+
+    public DeviationList(List<Deviation> deviationList) {
+        this.deviationList = deviationList;
+    }
+}
